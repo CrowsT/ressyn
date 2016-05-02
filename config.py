3bad7406b8308d3053ae07dd5d1e46e7d075c494
crows <pt.wenhan@gmail.com>
2016-05-02 20:44:53 +0800

    init projects over flask.

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+import private_config
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = private_config.SECRET_KEY
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = ''
+
+
+class TestConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = ''
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    "develop": DevelopConfig,
+    "production": ProductionConfig,
+
+    "default": Config,
+}
